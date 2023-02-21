f1da42f1771ee4a05a5688d26ecadfc7d880ff58
rnamocot <namocotrenie@gmail.comgit config --global user.name>
2023-02-21 22:05:57 +0800

    4th commit

--- config/ojt_database.php
+++ config/ojt_database.php
@@ -1,16 +1,18 @@
 <?php
-// function connectionDB(){  
-//     $host="localhost";
-//     $username="root";
-//     $password="";
-//     $database="teacher_ojt_system";
-//     $con =new mysqli($host, $username,$password, $database );
-//     if($con->connect_error){
-//        echo $con->connect_error;
-//     }else{
-//         return $con;
-//     }
-// }
+/*
+function connectionDB(){  
+    $host="localhost";
+    $username="root";
+    $password="";
+    $database="teacher_ojt_system";
+    $con =new mysqli($host, $username,$password, $database );
+    if($con->connect_error){
+       echo $con->connect_error;
+    }else{
+        return $con;
+    }
+}
+*/
 function connectionDB(){  
     $host="localhost";
     $username="u992206180_ojt";
